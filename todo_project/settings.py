32a47274fbe1f25e56652b17c4a8d6acd6278cf5
Shibil-Basith <84259186+Shibil-Basith@users.noreply.github.com>
2026-08-26 18:55:04 +0530

    Change PostgreSQL host to 'postgres'

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -53,7 +53,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB", "todo_db"),
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("POSTGRES_HOST", "127.0.0.1"),
+        "HOST": os.getenv("POSTGRES_HOST", "postgres"),
         "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
